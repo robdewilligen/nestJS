5e915c038b9cf85033b93c010fc0ad94582f2faf
Rob de Willigen <robdewilligen@outlook.com>
2024-02-27 17:08:29 +0100

    create a full controller
    created a full catconroller and a generated dog controller

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { CreateCatDto } from "./dto/create-cat.dto";
+import { UpdateCatDto } from "./dto/update-cat.dto";
 
 @Controller("cats")
 export class CatsController {
     @Post()
-    create(): string {
+    create(@Body() createCatDto: CreateCatDto): string {
         return "This action adds a new cat";
     }
     
@@ -12,9 +14,19 @@ export class CatsController {
         return "This action returns all cats";
     }
     
-    @Get(':id')
+    @Get(":id")
     findOne(@Param() params: any): string {
         console.log(params.id);
-        return `This action returns a #${params.id} cat`
+        return `This action returns a #${params.id} cat`;
+    }
+    
+    @Put(":id")
+    update(@Param("id") id: string, @Body() updateCatDto: UpdateCatDto) {
+        return `This action updates a #${id} cat`;
+    }
+    
+    @Delete(":id")
+    remove(@Param("id") id: string) {
+        return `This action removes a #${id} cat`;
     }
 }
