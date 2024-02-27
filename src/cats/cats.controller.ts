7781c8c2d2b56705dc0f0edf030f971f920a4d47
Rob de Willigen <robdewilligen@outlook.com>
2024-02-27 15:54:38 +0100

    initial commit
    first commit containing default files, cat controller and a cat DTO

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Param, Post } from "@nestjs/common";
+
+@Controller("cats")
+export class CatsController {
+    @Post()
+    create(): string {
+        return "This action adds a new cat";
+    }
+    
+    @Get()
+    findAll(): string {
+        return "This action returns all cats";
+    }
+    
+    @Get(':id')
+    findOne(@Param() params: any): string {
+        console.log(params.id);
+        return `This action returns a #${params.id} cat`
+    }
+}
