465e712447b6053f61fd76fc42181785370e41d6
Rob de Willigen <robdewilligen@outlook.com>
2024-02-27 17:13:28 +0100

    add controller to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { DogsModule } from './dogs/dogs.module';
+import { DogsModule } from "./dogs/dogs.module";
+import { CatsController } from "./cats/cats.controller";
 
 @Module({
-	imports: [DogsModule],
-	controllers: [AppController],
-	providers: [AppService],
+    imports: [DogsModule],
+    controllers: [AppController, CatsController],
+    providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+}
